1016afeb1aa7fcd018b20e2269249ed63c02ebee
Olga Cherenkova <alliancesema@gmail.com>
2023-08-29 21:50:36 +0200

    Четвертое задание выполнено

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,36 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
+let firstStudent = new Student("Петр", "мужской", 19);
+
+let secondStudent = new Student("Анна", "женский", 22);
+
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (this.hasOwnProperty('marks')) {
+        this.marks = this.marks.concat(marks);
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if(!this.hasOwnProperty('marks') || this.marks.length === 0) {
+    return 0;
+  }
+  let sum = this.marks.reduce((acc, item) => acc + item, 0);
+  let average = sum / this.marks.length;
+  return average;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
+
